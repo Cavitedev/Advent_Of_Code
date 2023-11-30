7cd9fb3fdab867a40dd959f1739cd205a9a12e60
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-30 17:04:57 +0100

    Submodule 2022

--- README.md
+++
@@ -1,3 +0,0 @@
-# Repo for Advent of Code problemas
-
-Problems solved on typescript and tested with jest for [Advent of Code](https://adventofcode.com/) problems.
--- src/2022/2/2.1.test.ts
+++
@@ -1,8 +0,0 @@
-import { rockPaperScissorsScoreCounter } from "./2.1.js";
-
-describe("2.1", () => {
-  it("First part, returns right score from input.txt", async () => {
-    const res = await rockPaperScissorsScoreCounter("input.txt");
-    expect(res).toBe(10941);
-  });
-});
